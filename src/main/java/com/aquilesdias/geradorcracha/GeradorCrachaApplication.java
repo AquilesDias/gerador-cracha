fb875963a9e06f7f3652e39a351090e0151f28d5
AquilesDias <kilesax@gmail.com>
2024-02-01 23:33:58 -0300

    first commit

---
+++ src/main/java/com/aquilesdias/geradorcracha/GeradorCrachaApplication.java
@@ -0,0 +1,13 @@
+package com.aquilesdias.geradorcracha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorCrachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorCrachaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aquilesdias/geradorcracha/GeradorCrachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquilesdias.geradorcracha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorCrachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
